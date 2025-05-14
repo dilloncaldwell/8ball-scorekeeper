60dd6f879a76af6d51da3a11a4a8759ab08be75a
Dillon Caldwell <dcaldwell1228@gmail.com>
2025-05-13 23:47:18 -0400

    moved GameProvider into its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import GameEndModal from './components/GameEndModal';
-// import GameSetupForm from './components/GameSetupForm';
-// import InningTracker from './components/InningTracker';
-// import PlayerTimer from './components/PlayerTimer';
-// import ScoreBoard from './components/ScoreBoard';
-// import TurnSwitcher from './components/TurnSwitcher';
-// import WinnerDisplay from './components/WinnerDisplay';
-// import { GameProvider } from './context/GameContext';
-
-// const App = () => {
-//   return (
-//     <GameProvider>
-//       <div className="app p-4 max-w-3xl mx-auto">
-//         <GameSetupForm />
-//         <ScoreBoard />
-//         <TurnSwitcher />
-//         <PlayerTimer />
-//         <InningTracker />
-//         <GameEndModal />
-//         <WinnerDisplay />
-//       </div>
-//     </GameProvider>
-//   );
-// };
-
-// export default App;
-
 import React from 'react';
 import './App.css';
 import GameUI from './components/GameUI';
